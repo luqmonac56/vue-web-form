8cb873e28fbe4e805ed15344153ddaa8486694d8
luqmonac56 <luqmonac56@gmail.com>
2021-08-18 15:09:40 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Form/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Form from "@/components/Form";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  Form
   }
 }
 </script>
@@ -22,5 +22,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body{
+  margin: 0;
+  background: #eee;
 }
 </style>
